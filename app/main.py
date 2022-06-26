e94b3dd37b4bce558a7a634a69aecf53f4ff375f
rmuraix <35632215+rmuraix@users.noreply.github.com>
2022-06-27 02:54:32 +0900

    add start message

---
+++ app/main.py
@@ -0,0 +1,15 @@
+from array import array
+import numpy as np
+
+def main():
+    starting()
+
+def starting():
+  # array = np.zeros(9, dtype='int').reshape((3,3))
+    print('################')
+    print('TIC-TAC-TOE GAME')
+    print('################')
+    print('This is provided under the MIT License.\nrepo: https://github.com/rmuraix/tic-tac-toe/')
+
+if __name__ == '__main__':
+    main()
